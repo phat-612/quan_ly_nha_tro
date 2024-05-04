db299ef3a4515c7c32d5218201be226eb51c25d3
CamHung1609 <camdaica20003@gmail.com>
2024-05-04 19:13:20 +0700

    Co-authored-by: Bich Phuong <bp21thg7@users.noreply.github.com>

--- src/public/js/room.js
+++ src/public/js/room.js
@@ -4,9 +4,11 @@ $(document).ready(function () {
   const inpXemarea = document.querySelector(".inpXemarea");
   const inpXemprice = document.querySelector(".inpXemprice");
   const inpXemdescription = document.querySelector(".inpXemdescription");
-  const inpXemisEmpty = document.querySelector(".inpXemisEmpty");
   const inpXemcapacity = document.querySelector(".inpXemcapacity");
   const updateBtninModal = document.querySelector(".updateBtninModal");
+  const cancelBtninModal = document.querySelector(".cancelBtninModal");
+  const saveBtninModal = document.querySelector(".saveBtninModal");
+  const inpXemisEmpty = document.querySelector(".inpXemisEmpty");
 
   $(".btnXemRoom").click((e) => {
     const button = $(e.currentTarget);
@@ -18,7 +20,19 @@ $(document).ready(function () {
     const area = button.attr("data-bs-area");
     const capacity = button.attr("data-bs-capacity");
     const description = button.attr("data-bs-description");
-    const idAmenities = [];
+    const idAmeniti_length = button.attr("data-bs-idAmeniti_length");
+
+    const idAmenitiesArr = [];
+    const idAmeniti = {};
+    for (let i = 0; i < idAmeniti_length; i++) {
+      const key = "idAmeniti" + i;
+      const value = button.attr("data-bs-idAmenities" + i);
+
+      idAmeniti[key] = value;
+
+      console.log(idAmeniti[key]);
+    }
+
     inpXemroomNumber.value = roomNumber;
     inpXemfloor.value = floor;
     inpXemarea.value = area;
@@ -33,6 +47,31 @@ $(document).ready(function () {
 
     updateBtninModal.addEventListener("click", () => {
       document.querySelector(".titleXCT").textContent = "Sửa Chi Tiết";
+      inpXemroomNumber.removeAttribute("disabled");
+      inpXemfloor.removeAttribute("disabled");
+      inpXemarea.removeAttribute("disabled");
+      inpXemprice.removeAttribute("disabled");
+      inpXemdescription.removeAttribute("disabled");
+      inpXemcapacity.removeAttribute("disabled");
+      $(".checkboxAmeniti").prop("disabled", false);
+
+      updateBtninModal.setAttribute("hidden", "true");
+      cancelBtninModal.removeAttribute("hidden");
+      saveBtninModal.removeAttribute("hidden");
+
+      cancelBtninModal.addEventListener("click", () => {
+        updateBtninModal.removeAttribute("hidden");
+        cancelBtninModal.setAttribute("hidden", "true");
+        saveBtninModal.setAttribute("hidden", "true");
+        document.querySelector(".titleXCT").textContent = "Xem Chi Tiết";
+        inpXemroomNumber.setAttribute("disabled", "true");
+        inpXemfloor.setAttribute("disabled", "true");
+        inpXemarea.setAttribute("disabled", "true");
+        inpXemprice.setAttribute("disabled", "true");
+        inpXemdescription.setAttribute("disabled", "true");
+        inpXemcapacity.setAttribute("disabled", "true");
+        $(".checkboxAmeniti").prop("disabled", true);
+      });
     });
   });
 });
